e9112fdb007cccc16356dd0d8ee0d450748917bb
darioospina <65924368+darioospina@users.noreply.github.com>
2022-10-11 17:53:00 -0600

    Added my name

--- courseproject/src/App.js
+++ courseproject/src/App.js
@@ -2,7 +2,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <H1>My name is Abdul</H1>
+      <h1>My name is Abdul</h1>
+      <h1>My name is Dario</h1>
     </div>
   );
 }
