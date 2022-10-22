e7a2f829d4502232102ffa067518a6ad9ba4a69c
darioospina <65924368+darioospina@users.noreply.github.com>
2022-10-22 15:14:15 -0600

    Modified Login page

--- courseproject/src/Pages/Login.js
+++ courseproject/src/Pages/Login.js
@@ -1,14 +1,33 @@
 import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 // Abdul
 export const Login = (props) => {
+    const navigate = useNavigate();
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(email);
 
+        e.preventDefault();
+        console.log(email + " " + password)
+        console.log(students.some(e => e.email == email))
+        console.log(students.some(e => e.password == password))
+        console.log(students.includes(password))
+        if(students.some(e => e.email == email) && students.some(e => e.password == password)) {
+            navigate("../Profile");
+        } else {
+            alert("The user and password are not in our database")
+        }
     }
+
+    const students = [
+        {email: "abc@gmail.com",
+        password: 123456},
+        {email: "zaz@hotmail.com",
+        password: 123456},
+        {email: "sample",
+        password: "sample"}
+    ]
 
     return(
         <div className="auth-form-container">
@@ -18,7 +37,7 @@ export const Login = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlfor="password">Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="***********" id="password" name="password" />
-                <button type="submit" onClick={() => props.onFormSwitch('')}>Login</button>
+                <button type="submit" onClick={handleSubmit}>Login</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
             
