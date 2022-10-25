7dc9faf7e37289712bc5bf033c86bacc97a32858
gafmalix <79617087+gafmalix@users.noreply.github.com>
2022-10-25 14:57:07 -0600

    new

--- courseproject/src/Pages/Login.js
+++ courseproject/src/Pages/Login.js
@@ -4,20 +4,20 @@ import students from "../JSON/students.json"
 import {Link} from "react-router-dom";
 
 // Abdul
-const Login = ({setUser}, props) => {
+const Login = ({setUser}) => {
     const navigate = useNavigate();
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     
     const handleSubmit = (e) =>{
         e.preventDefault();
-        if(students.some(e => e.email === email) && students.some(e => e.password === password)) {
+        if(students.some(e => e.email == email) && students.some(e => e.password == password)) {
             navigate("../Profile", {state:{email}});
         } else {
             alert("The user and password are not in our database")
         }
     }
-   
+
 
     return(
         <div className="auth-form-container">
@@ -27,7 +27,7 @@ const Login = ({setUser}, props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlfor="password">Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="***********" id="password" name="password" />
-                <button type="submit" onClick={() => props.onFormSwitch('')}>Login</button>
+                <button type="submit" onClick={handleSubmit}>Login</button>
 
             </form>
             {/*<button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>*/}
